4ce0442fb3ca6476693a78d1d14ebb1ca83d743c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2020-05-22 04:10:36 +0900

    Improve the ocamldoc comments

--- source/terminal.mli
+++ source/terminal.mli
@@ -3,21 +3,24 @@
 (** {6 Title} *)
 
 val title: string -> (unit -> 'a) -> 'a
-(** [title t f] saves old window title,
-    sets it to given local encoded string [t], and restores it. *)
+(** [title t f] saves the old window title, sets it to a given string [t],
+    and restores it.
+    In Windows, the parameter should be encoded as the active code page. *)
 
 val title_utf8: string -> (unit -> 'a) -> 'a
-(** [title_utf8 t f] saves old window title,
-    sets it to given UTF-8 encoded string [t], and restores it.
+(** [title_utf8 t f] saves the old window title, sets it to a given UTF-8
+    encoded string [t], and restores it.
     In POSIX, it's same as [set_title]. *)
 
 (** {6 Color type and values} *)
 
 type color = private int
+(** Color. The representation is not portable. *)
 
 val system_16: red:int -> green:int -> blue:int -> intensity:int -> color
-(** The 16 system colors. It's discriminated whether each value of [red],
-    [green], [blue], or [intensity] is 0 or not 0. *)
+(** The 16 system colors.
+    It's discriminated whether each value of [red], [green], [blue], or
+    [intensity] is 0 or not. *)
 
 val black: color
 val dark_red: color
@@ -48,31 +51,33 @@ val grayscale: float -> color
 (** {6 Event} *)
 
 type event = private string
-(** Event. One of inputed char or string, the terminal window is resized,
-    any special key is typed, any mouse button is clicked or others. *)
+(** Event.
+    One of an inputed char or string, the terminal window is resized,
+    any special key is typed, any mouse button is clicked, or others. *)
 
 val escape_sequence_of_event: event -> string
-(** Represent given event as escape sequence. *)
+(** Represent a given event as escape sequence. *)
 
 val is_char: event -> bool
-(** Check whether given event contains a char or not. *)
+(** Check whether a given event contains some char or not. *)
 
 val char_of_event: event -> char
-(** Retrun a char of given event. *)
+(** Retrun the char of a given event. *)
 
 val is_string: event -> bool
-(** Check whether given event contains a string(non-escape sequence) or not. *)
+(** Check whether a given event contains some string(non-escape sequence) or
+    not. *)
 
 val string_of_event: event -> string
-(** Retrun a string of given event. *)
+(** Retrun the string of a given event. *)
 
 val is_resized: event -> bool
-(** Check whether given event means terminal window has been resized.
-    It install sigwinch handler (in POSIX) or change console mode (in Windows)
-    when calling [size], [set_size], [view] or [screen]. *)
+(** Check whether a given event means the terminal window has been resized.
+    It installs the sigwinch handler (in POSIX) or change the console mode (in
+    Windows) when calling [size], [set_size], [view], or [screen]. *)
 
 val size_of_event: event -> int * int
-(** Return new window size of given event. *)
+(** Return the new window size of a given event. *)
 
 type key = [
 	| `up
@@ -103,18 +108,17 @@ type shift_state = private int
 (** Set of [shift_key]. *)
 
 val is_key: event -> bool
-(** Check whether given event contains a key or not. *)
+(** Check whether a given event contains some key or not. *)
 
 val key_of_event: event -> [key | `unknown]
-(** Retrun a key of given event. *)
+(** Retrun the key of a given event. *)
 
 val shift_of_event: event -> shift_state
-(** Retrun shift state of given event.
-    [shift_of_event] works for key event and also mouse clicked event.
-    But modifier+Click is normally unavailable because popup menu. *)
+(** Retrun the shift state of a given event.
+    [shift_of_event] works for key events and also mouse button events. *)
 
 type shift_key = private int
-(** One of shift key, control key or alt(meta) key. *)
+(** One of shift key, control key, or alt(meta) key. *)
 
 val empty: shift_state
 (** The empty set. *)
@@ -128,7 +132,7 @@ val mem: shift_key -> shift_state -> bool
 (** [mem a s] is true if [a] is included in [s]. *)
 
 external add: shift_key -> shift_state -> shift_state = "%orint"
-(** [add a s] returns a set containing all elements of [s] and [a]. *)
+(** [add a s] returns a set containing all elements of [s] plus [a]. *)
 
 type button = [
 	| `button1
@@ -140,13 +144,13 @@ type button = [
 (** Mouse button. *)
 
 val is_clicked: event -> bool
-(** Check whether given event means a mouse button is clicked. *)
+(** Check whether a given event means some mouse button is clicked. *)
 
 val button_of_event: event -> [button | `unknown]
-(** Return a clicked mouse button of given event. *)
+(** Return the clicked mouse button of a given event. *)
 
 val position_of_event: event -> int * int
-(** Retrun mouse position of given event. *)
+(** Retrun the pointed position of a given event. *)
 
 (** {6 Operations for Unix.file_descr} *)
 
@@ -198,7 +202,7 @@ module Descr: sig
 		'a
 	
 	val output: file_descr -> bytes -> int -> int -> unit
-	(** Same as [Unix.write], but return type is unit. *)
+	(** Same as [Unix.write], but the returned type is unit. *)
 	
 	val output_substring: file_descr -> string -> int -> int -> unit
 	(** Same as output but take a string instead of a bytes. *)
@@ -210,7 +214,7 @@ module Descr: sig
 	val output_string_utf8: file_descr -> string -> unit
 	
 	val output_newline: file_descr -> unit -> unit
-	(** Write '\n' in POSIX, or write '\r\n' in Windows. *)
+	(** Write "\n" (in POSIX) or "\r\n" (in Windows). *)
 	
 	val mode:
 		file_descr ->
@@ -227,33 +231,33 @@ module Descr: sig
 	val input_line_utf8: file_descr -> string
 	
 	val is_empty: file_descr -> bool
-	(** Check whether given file descripter is empty or has any events. *)
+	(** Check whether a given file descripter is empty or having any events. *)
 	
 	val input_event: file_descr -> event
-	(** Read one event from given file descripter. *)
+	(** Read one event from a given file descripter. *)
 	
 end
 (** Operations for Unix.file_descr.
-    [Descr.anyf fd] is equal to [anyf (Unix.out_channel_of_descr fd)]
-    or [anyf (Unix.in_channel_of_descr fd)].
-    This module has additional output function [output_newline],
-    and additional input functions [is_empty] and [input_event]
-    for event handling. *)
+    [Descr.any_func fd] is equal to [any_func (Unix.out_channel_of_descr fd)]
+    or [any_func (Unix.in_channel_of_descr fd)].
+    This module has the additional output function [output_newline], and the
+    additional input functions [is_empty] and [input_event] for event handling.
+    *)
 
 (** {6 Operations for output channel} *)
 
 val is_terminal_out: out_channel -> bool
-(** [is_terminal_out oc] returns true if given output channel is associated
-    to terminal. *)
+(** [is_terminal_out oc] returns true if a given output channel is associated
+    to the terminal. *)
 
 val size: out_channel -> int * int
-(** [size oc] gets size of current screen buffer. *)
+(** [size oc] gets the size of the current screen buffer. *)
 
 val set_size: out_channel -> int -> int -> unit
-(** [set_size oc width height] sets size of current screen buffer. *)
+(** [set_size oc width height] sets the size of the current screen buffer. *)
 
 val view: out_channel -> int * int * int * int
-(** [view oc] gets range of current view port that is a part of
+(** [view oc] gets the range of the current view port that is a part of the
     current screen buffer. *)
 
 val position: out_channel -> int * int
@@ -263,11 +267,11 @@ val set_position: out_channel -> int -> int -> unit
 (** [set_position oc x y] moves the cursor in absolute coordinates. *)
 
 val move: out_channel -> int -> int -> unit
-(** [move oc x y] moves the cursor in relative coordinates
-    from current position. *)
+(** [move oc x y] moves the cursor in relative coordinates from the current
+    position. *)
 
 val move_to_bol: out_channel -> unit -> unit
-(** [move_to_bol oc ()] moves the cursor to the begin of line. *)
+(** [move_to_bol oc ()] moves the cursor to the beginning of the line. *)
 
 val color:
 	out_channel ->
@@ -282,23 +286,24 @@ val color:
 	unit ->
 	unit
 (** [color oc ~reset ~bold ~underscore ~blink ~reverse ~concealed ~foreground
-    ~background ()] sets a color to write new text. *)
+    ~background ()] sets the color for writing. *)
 
 val save: out_channel -> (unit -> 'a) -> 'a
-(** [save oc f] saves and restores current position and color. *)
+(** [save oc f] saves and restores the current position and the color. *)
 
 val clear_screen: out_channel -> unit -> unit
-(** [clear_screen oc ()] clears all of given screen buffer. *)
+(** [clear_screen oc ()] clears all of a given screen buffer. *)
 
 val clear_eol: out_channel -> unit -> unit
-(** [clear_eol oc ()] clears from the cursor position to the end of line. *)
+(** [clear_eol oc ()] clears from the cursor position to the end of the line.
+    *)
 
 val clear_line: out_channel -> unit -> unit
-(** [clear_line oc ()] is a shorthand of [move_to_bol oc ()]
-    and then [clear_eol oc ()]. *)
+(** [clear_line oc ()] is a combination of [move_to_bol oc ()] and
+    [clear_eol oc ()]. *)
 
 val scroll: out_channel -> int -> unit
-(** [scroll oc y] scrolls contents in view port. *)
+(** [scroll oc y] scrolls the contents in the current view port. *)
 
 val show_cursor: out_channel -> bool -> unit
 (** [show_cursor oc flag] show or hide the cursor. *)
@@ -313,37 +318,36 @@ val screen:
 	?wrap:bool ->
 	(out_channel -> 'a) ->
 	'a
-(** [screen oc ~size ~cursor ~wrap f] saves current screen buffer,
-    uses new screen buffer and restore old it.
+(** [screen oc ~size ~cursor ~wrap f] saves the current screen buffer,
+    uses new screen buffer, and restores it.
     If [~size] is given, same as [set_size oc size].
     If [~cursor] is given, same as [show_cursor oc cursor].
     If [~wrap:w] is given, same as [wrap oc w]. *)
 
 val output_substring_utf8: out_channel -> string -> int -> int -> unit
-(** Write a part of a UTF-8 encoded string to the given output channel.
+(** Write a part of a UTF-8 encoded string to a given output channel.
     In POSIX, It's same as [Pervasives.output]. *)
 
 val output_string_utf8: out_channel -> string -> unit
-(** Write a UTF-8 encoded string to the given output channel.
+(** Write a UTF-8 encoded string to a given output channel.
     In POSIX, It's same as [Pervasives.output_string]. *)
 
 (** {6 Operations for input channel} *)
 
 val is_terminal_in: in_channel -> bool
-(** [is_terminal_in ic] returns true if given input channel is associated
-    to terminal. *)
+(** [is_terminal_in ic] returns true if a given input channel is associated to
+    the terminal. *)
 
 val buffered_in: in_channel -> int
-(** Return size of buffered contents of given input channel
-    that has not been read yet. *)
+(** Return the size of the buffered contents of a given input channel that has
+    not been read yet. *)
 
 val buffered_line_in: in_channel -> int
-(** Return size of buffered contents of given input channel
-    that has not been read yet,
-    from current position until ['\n'].
-    If returned value is [max_int], no ['\n'] is in buffer.
-    It means current line is continuing.
-    Otherwide, all of current line is buffered. *)
+(** Return the size of the buffered contents of a given input channel that has
+    not been read yet, from current position until ['\n'].
+    If a returned value is [max_int], no ['\n'] is in the buffer.
+    It means the current line is continuing.
+    Otherwide, all of the current line is buffered. *)
 
 val mode:
 	in_channel ->
@@ -353,25 +357,25 @@ val mode:
 	?mouse:bool ->
 	(unit -> 'a) ->
 	'a
-(** [mode ic ~echo ~canonical ~control_c ~mouse f] saves, changes and restores
-    mode of given input channel.
-    If [~echo] is false, disable echoing.
-    If [~canonical] is false, disable line editing.
-    If [~control_c] is false, ignore Ctrl+C.
-    (Use [Sys.catch_break] to handle Ctrl+C as exception.)
-    If [~mouse] is true, get mouse events. *)
+(** [mode ic ~echo ~canonical ~control_c ~mouse f] saves, changes, and restores
+    the mode of a given input channel.
+    If [~echo] is given, enables or disables echoing.
+    If [~canonical] is given, enables or disables line editing.
+    If [~control_c] is given, accepts or ignores Ctrl+C.
+    (Use [Sys.catch_break] to handle Ctrl+C as the exception.)
+    If [~mouse] is given, enables or disables mouse button events. *)
 
 val input_line_utf8: in_channel -> string
-(** Read from the given input channel until a newline.
+(** Read from a given input channel until a newline.
     And return a UTF-8 encoded string.
     In POSIX, It's same as [Pervasives.input_line]. *)
 
 (** {6 Miscellany} *)
 
 val utf8_of_locale: string -> string
-(** In windows, encode string from active code page to UTF-8.
+(** In windows, encode a string from the active code page to UTF-8.
     In POSIX, It's no effect. *)
 
 val locale_of_utf8: string -> string
-(** In windows, encode string from UTF-8 to active code page.
+(** In windows, encode a string from UTF-8 to the active code page.
     In POSIX, It's no effect. *)
