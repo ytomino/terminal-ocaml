e9ef66b3590593c9a08778fee745887618c18d6f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-03-16 18:37:10 +0900

    Use "%B" insteaf of "%b" for printf in examples
    It is for backward compatibility, and has been deprecated explicity
    since OCaml 4.00.

--- examples/info.ml
+++ examples/info.ml
@@ -1,11 +1,11 @@
 let isatty = Terminal.is_terminal_out stdout in
-Printf.printf "isatty(stdout): %b\n" isatty;;
+Printf.printf "isatty(stdout): %B\n" isatty;;
 
 let isatty = Terminal.is_terminal_out stderr in
-Printf.printf "isatty(stderr): %b\n" isatty;;
+Printf.printf "isatty(stderr): %B\n" isatty;;
 
 let isatty = Terminal.is_terminal_in stdin in
-Printf.printf "isatty(stdin): %b\n" isatty;;
+Printf.printf "isatty(stdin): %B\n" isatty;;
 
 let w, h = Terminal.size stdout in
 Printf.printf "size: %dx%d\n" w h;;
@@ -17,4 +17,4 @@ let x, y = Terminal.position stdout in
 Printf.printf "position: (%d, %d)\n" x y;;
 
 let c256 = Terminal.supports_256 () in
-Printf.printf "256 color: %b\n" c256;;
+Printf.printf "256 color: %B\n" c256;;
